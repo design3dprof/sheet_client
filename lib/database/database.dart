657d08b3ff81839b9559879be0cabf63576261e8
design3dprof <161627740+design3dprof@users.noreply.github.com>
2024-10-21 20:14:06 +0200

    Update database.dart

--- lib/database/database.dart
+++ lib/database/database.dart
@@ -19,63 +19,15 @@ class Database {
           .get()
           .then((QuerySnapshot querySnapshot) {
 
-        querySnapshot.docs.forEach((doc) {
-          List<Ciklus> ciklusok = [];
-
-          doc['ciklus'].forEach((elem) {
-            Ciklus ciklus = Ciklus(
-              operationID: elem['Operation ID'],
-              operationDesc: elem['Operation Desc'],
-              operationToolNr: elem['Operation Tool Nr'],
-              operationToolDesc: elem['Operation Tool Desc'],
-              operationTime: elem['Operation Time'],
-              operationSpeed: checkDouble(elem['Operation Speed']),
-              operationFeed: checkDouble(elem['Operation Feed']),
-              operationMaxZ: checkDouble(elem['Operation Max Z']),
-              operationMinZ: checkDouble(elem['Operation Min Z']),
-              operationStockToLeaveXY: checkDouble(elem['Operation Stock To Leave XY']),
-              operationStockToLeaveZ: checkDouble(elem['Operation Stock To Leave Z']),
-              strategy: elem['Operation Strategy'],
-              tolerance: checkDouble(elem['Operation Tolerance']),
-              coolant: elem['Coolant'],
-              toolDiameter: checkDouble(elem['Tool Diameter']),
-              cornerRadius: checkDouble(elem['Tool Corner Radius']),
-              toolType: elem['Tool Type'],
-              maxStepdown: checkDouble(elem['Maximum Stepdown']),
-              maxStepover: checkDouble(elem['Maximum Stepover']),
-              cuttingDistance: checkDouble(elem['Cutting Distance']),
-              rapidDistance: checkDouble(elem['Rapid Distance']),
-              notes: elem['Operation Notes'],
-            );
-            ciklusok.add(ciklus);
-          });
-
-          operation = Operation(
-            program: doc['Program'],
-            programName: doc["Program Name"],
-            programPartName: doc["Program Part Name"],
-            programComment: doc["Program Comment"],
-            programCycleTime: doc["Program Cycle Time"],
-            dimensionX: checkDouble(doc['Dimension X']),
-            dimensionY: checkDouble(doc['Dimension Y']),
-            dimensionZ: checkDouble(doc['Dimension Z']),
-            date: doc["Date"],
-            user: doc['User'],
-            ciklus: ciklusok,
-          );
-          operationList.add(operation!);
-        });
+        operationList = querySnapshot.docs
+            .map((doc) => Operation.fromJson(doc.data() as Map<String, dynamic>))
+            .toList();
       });
     } catch (e) {
       Get.snackbar('Read Data Error', '$e');
       //print(e);
     }
     return operationList;
-  }
-
-
-  static double checkDouble(dynamic value) {
-    return value is int ? value.toDouble() : value;
   }
 
   readImageFire(name) async {
